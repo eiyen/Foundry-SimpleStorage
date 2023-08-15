d54e23b203c35094bebe158e50bd247a4612e6de
eiyen <937915771@qq.com>
2023-08-15 17:02:52 +0800

    refactor: 优化代码风格

--- src/SimpleStorage.sol
+++ src/SimpleStorage.sol
@@ -3,27 +3,26 @@
 pragma solidity 0.8.19;
 
 contract SimpleStorage {
-    uint256 myFavoriteNumber;
-
     struct Person {
         uint256 favoriteNumber;
         string name;
     }
-    
-    Person[] public listOfPeople;
 
+    uint256 private myFavoriteNumber;
+    Person[] public listOfPeople;
     mapping(string => uint256) public nameToFavoriteNumber;
 
     function store(uint256 _favoriteNumber) public {
-        myFavoriteNumber = _favoriteNumber;
-    }
 
-    function retrieve() public view returns (uint256) {
-        return myFavoriteNumber;
+        myFavoriteNumber = _favoriteNumber;
     }
 
     function addPerson(string memory _name, uint256 _favoriteNumber) public {
         listOfPeople.push(Person(_favoriteNumber, _name));
         nameToFavoriteNumber[_name] = _favoriteNumber;
+    }
+
+    function getMyFavoriteNumber() public view returns (uint256) {
+        return myFavoriteNumber;
     }
 }
